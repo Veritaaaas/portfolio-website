a20e4a6731a338924f2c56a20d85cb961dd2df98
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-05-29 01:36:44 +0800

    Add parallax effect

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,17 @@
 import Landing_page from "./components/landing_page/Landing_page.jsx";
+import About from "./components/tools/About.jsx";
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
   return (
-    <div>
-      <Landing_page />
-    </div>
+    <Parallax pages={4}>
+      <ParallaxLayer offset={0}>
+        <Landing_page />
+      </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={0.6}>
+        <About />
+      </ParallaxLayer>
+    </Parallax>
   );
 }
 
