fe69d86773af5460319ff1635c22f5c18fe1d6a8
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-07-01 21:19:32 +0800

    Add Projects component and Footer component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,23 @@
 import Landing_page from "./components/landing_page/Landing_page.jsx";
 import About from "./components/tools/About.jsx";
+import Projects from "./components/projects/Projects.jsx";
+import Footer from "./components/Footer.jsx";
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
   return (
-    <Parallax pages={4}>
+    <Parallax pages={4.5}>
       <ParallaxLayer offset={0}>
         <Landing_page />
       </ParallaxLayer>
-      <ParallaxLayer offset={1} speed={0.6}>
+      <ParallaxLayer offset={1} speed={0.4}>
         <About />
+      </ParallaxLayer>
+      <ParallaxLayer offset={2} speed={0.7}>
+        <Projects />
+      </ParallaxLayer>
+      <ParallaxLayer offset={3.9} speed={0}>
+        <Footer />
       </ParallaxLayer>
     </Parallax>
   );
---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import { PiHandshakeFill } from "react-icons/pi";
+
+
+function Footer() {
+  return (
+    <div className="bg-[#FDFDFD] footer">
+        <div className="flex justify-center">
+            <div className="flex flex-col gap-2 items-center">
+                <div className="rounded-full bg-[#F6F5F4] aspect-square h-[200px] max-w-[200px] flex items-center justify-center">
+                    <PiHandshakeFill className="text-black text-[100px]"/>
+                </div>
+                <h1 className="text-[60px] bg-gradient-to-b from-[#0B0B0B] to-[#666666] inline-block text-transparent text-center bg-clip-text inter w-[45rem]">Tell me about your next project</h1>
+                <a href="https://www.linkedin.com/in/rodney-maisog/" className="p-2 pl-4 pr-4 bg-[#0B0B0B] text-white rounded-3xl">Contact Me</a>
+            </div>
+        </div>
+       
+    </div>
+  );
+}
+
+export default Footer;
